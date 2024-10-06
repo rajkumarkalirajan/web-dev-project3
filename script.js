46dac1ad280d3f742a0d9c469f88372d1c6b36d0
rajkumarkalirajan <rkalirajan@hawk.iit.edu>
2024-10-05 23:23:44 -0400

    added image in the media folder

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 function init() {
-//alert(;it works');
+alert('It works!');
+
 var el = document.getElementByid('canvas');
 var my Location = new google.maps.LatLng(55.750471, 37.622532)
 var mapOptions = {
 		center: myLocation,
 		zoom: 18,
 		mapTypeId: google.maps.MapsTypeId.SATELLITE,
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-}
 };
 
 var myMap = new google.maps.Map(el, mapOptions);
+
 var marker = new google.maps.Marker({
 	position: myLocation,
 	map: myMap,
 	animation: google.maps.Animation.BOUNCE,
-	icon: 'iit-icon.png'
+	icon:'Kremlin.png'
 });
 
-var contentString = '<h1>IIT Perlsterin Hall</h1><p>Lorem ipsum dolor sit amet, content String
+var contentString = '<h1>Moscow_Kremmlin</h1><p>Kremlin is a fortified complex in Moscow</p>';
+var infowindow = new google.maps.Inflowindow({
+		content: contentString
 });
 
 }
 	google.maps.event.addListener(marker, 'mouseover', function() {
 		infowindow.open(myMap, marker);
 	});
-	
+}	
 google.maps.event.adDomListener(window, 'load', init);
 
 
