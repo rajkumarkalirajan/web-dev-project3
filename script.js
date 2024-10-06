2ee1903b7231da87b69fc9ab57b4b744f1d3a855
rajkumarkalirajan <rkalirajan@hawk.iit.edu>
2024-10-06 11:15:04 -0400

    updated the image path

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function initMap() {
         position: myLocation,
         map: myMap,
         animation: google.maps.Animation.BOUNCE,
-        icon: 'moscow.png' 
+        icon:'https://via.placeholder.com/30' 
     });
 
     var contentString = '<h1>Moscow Kremlin</h1><p>Kremlin is a fortified complex in Moscow</p>';
