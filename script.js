32ed2c2e7e9eef4c35276d8d563e45a0e184152d
rajkumarkalirajan <rkalirajan@hawk.iit.edu>
2024-10-06 15:14:18 -0400

    updated image url

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function initMap() {
         position: myLocation,
         map: myMap,
         animation: google.maps.Animation.BOUNCE,
-        icon:https://maps.app.goo.gl/AoL9Jm2hudTEgCPo7
+        icon:'https://upload.wikimedia.org/wikipedia/commons/e/e6/Moscow_Kremlin%2C_Spasskaya_Tower_%28cropped%29.jpg'
     });
 
     var contentString = '<h1>Moscow Kremlin</h1><p>Kremlin is a fortified complex in Moscow</p>';
