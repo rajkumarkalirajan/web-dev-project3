de1f996153bddf2c88dcd0623afceb17452dc9a6
rajkumarkalirajan <rkalirajan@hawk.iit.edu>
2024-10-06 11:08:39 -0400

    updated script.js

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-function init() {
-alert('It works!');
+function initMap() {
+    alert('It works!');
 
-var el = document.getElementById('map');
-var myLocation = new google.maps.LatLng(55.750471, 37.622532)
-var mapOptions = {
-		center: myLocation,
-		zoom: 18,
-		mapTypeId: google.maps.MapsTypeId.SATELLITE,
-};
+    var el = document.getElementById('map');
+    var myLocation = new google.maps.LatLng(55.750471, 37.622532);
+    var mapOptions = {
+        center: myLocation,
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.SATELLITE, 
+    };
 
-var myMap = new google.maps.Map(el, mapOptions);
+    var myMap = new google.maps.Map(el, mapOptions);
 
-var marker = new google.maps.Marker({
-	position: myLocation,
-	map: myMap,
-	animation: google.maps.Animation.BOUNCE,
-	icon:'moscow.png'
-});
+    var marker = new google.maps.Marker({
+        position: myLocation,
+        map: myMap,
+        animation: google.maps.Animation.BOUNCE,
+        icon: 'moscow.png' 
+    });
 
-var contentString = '<h1>Moscow_Kremmlin</h1><p>Kremlin is a fortified complex in Moscow</p>';
-var infowindow = new google.maps.Inflowindow({
-		content: contentString
-});
+    var contentString = '<h1>Moscow Kremlin</h1><p>Kremlin is a fortified complex in Moscow</p>';
+    var infowindow = new google.maps.InfoWindow({ 
+        content: contentString
+    });
 
+    google.maps.event.addListener(marker, 'mouseover', function() {
+        infowindow.open(myMap, marker);
+    });
+
+    google.maps.event.addListener(marker, 'mouseout', function() {
+        infowindow.close(); 
+    });
 }
-	google.maps.event.addListener(marker, 'mouseover', function() {
-		infowindow.open(myMap, marker);
-	});
-}	
-google.maps.event.addDomListener(window, 'load', init);
+
+google.maps.event.addDomListener(window, 'load', initMap);
 
 
 
