216aa945419596c7440af36acae079d50a35524b
rajkumarkalirajan <rkalirajan@hawk.iit.edu>
2024-10-06 23:05:58 -0400

    updates slider js code

--- slider.js
+++ slider.js
@@ -2,7 +2,7 @@ let currentSlide = 0;
 
 function changeSlide(direction) {
 	const slides = document.querySelectorAll('.slide');
-	slides[currentSlide).classList.remove("active");
+	slides[currentSlide].classList.remove("active");
 
 	currentSlide = (currentSlide + direction + slides.length) % slides.length;
 	slides[currentSlide].classList.add('active');
